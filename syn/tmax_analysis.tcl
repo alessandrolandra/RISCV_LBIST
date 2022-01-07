11b51475160ac2d2a734f4af5090a8bfb17a3154
Michelangelo Bartolomucci <3181748+mkexc@users.noreply.github.com>
2022-01-07 11:35:23 +0100

    Revert "MOD: tmax analysis for scan compressed"
    This reverts commit e357ab8fa2bd8377f8bd1aa65cd7cee5e7e2c0e0.

--- syn/tmax_analysis.tcl
+++ syn/tmax_analysis.tcl
@@ -8,8 +8,8 @@ set library  "../techlib/NangateOpenCellLibrary.v"
 set coreNet  "../output/riscv_core_scan64.v"
 set entity   "riscv_core_0_128_1_16_1_1_0_0_0_0_0_0_0_0_0_3_6_15_5_1a110800"
 # stil file (compressor: riscv_core_scanXX_cmpYY.spf) (normal: riscv_core_scanXX
-set coreStil "../output/riscv_core_scan64_cmp320.spf"
-#set coreStil "../output/riscv_core_scan64.spf"
+#set coreStil "../output/riscv_core_scan20_cmp60.spf"
+set coreStil "../output/riscv_core_scan64.spf"
 #output atgp patterns
 set outPatterns "../output/atpg_gen_patt.spf"
 
@@ -51,9 +51,10 @@ set_atpg -abort_limit 200
 #full sequential limit (higher = more effort)
 set_atpg -full_seq_abort_limit 50
 run_atpg -auto_compression
-set_faults -summary verbose -fault_coverage
-report_summaries
+#set_faults -summary verbose -fault_coverage
+#report_summaries
 #report_patterns -all
-write_patterns $outPatterns -replace -format stil -first 0 -last 9 
+
+#write_patterns $outPatterns -format stil
 
 #quit
